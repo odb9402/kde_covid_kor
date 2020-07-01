8e6b293a4d20cd26fe2752b69cb02fe55be1c039
DongpinOh <dhehdqls@gmail.com>
2020-07-02 00:37:52 +0900

    Add tests

--- src/kde_covid_kor/test/KernelDensity.java
+++ src/kde_covid_kor/test/KernelDensity.java
@@ -1,7 +1,17 @@
 package kde_covid_kor.test;
 
 import java.util.Arrays;
+import java.util.ArrayList;
 
+import org.apache.commons.math3.geometry.Point;
+import org.apache.log4j.Level;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.graphx.*;
+import org.apache.spark.sql.SparkSession;
+
+import org.apache.log4j.Logger;
+import org.apache.spark.api.java.JavaRDD;
 
 
 public class KernelDensity  {
@@ -19,7 +29,7 @@ public class KernelDensity  {
      * The kernel -- a symmetric but not necessarily positive function that
      * integrates to one. Here we just Gaussian density function.
      */
-    private GaussianDistribution gaussian;
+    //private GaussianDistribution gaussian;
     /*
      * h > 0 is a smoothing parameter called the bandwidth.
      */
@@ -27,40 +37,107 @@ public class KernelDensity  {
     /**
      * The mean value.
      */
+    private double weight_sum;
+     
+
     private double mean;
     /**
      * The standard deviation.
      */
+
+    private double sdy;
+    private double variancey;
+
     private double sd;
     /**
      * The variance.
      */
     private double variance;
 
+    private double[][] data;
     /**
      * Constructor. The bandwidth of kernel will be estimated by the rule of thumb.
      * @param x the samples to estimate the density function.
      */
-    public KernelDensity(double[][] data) {
+    private JavaRDD<double[]> myRdd;
+    
+    public KernelDensity(JavaSparkContext sc,double[][] data ) {
+        
+        
+        double[][] data_ = new double[data.length][3];
         double[] x = new double[data.length];
         double[] y = new double[data.length];
         for (int i =0 ;i<data.length; i++){
             x[i]=data[i][0];
             y[i]=data[i][1];
+            data_[i][0] = data[i][0];
+            data_[i][1] = data[i][1];
+            data_[i][2] = 1;
         }
         this.x = x;
         this.y = y;
+        this.data = data_;
         this.mean = mean(x);
+        this.weight_sum = data.length;
         this.variance = Math.pow(sd(x),2);
         this.sd = Math.sqrt(variance);
+        this.variancey = Math.pow(sd(y),2);
+        this.sdy = Math.sqrt(variancey);
+        
 
         Arrays.sort(x);
         Arrays.sort(y);
 
         int n = x.length;
-        double iqr = x[n*3/4] - x[n/4];
-        h = 1.06 * Math.min(sd, iqr/1.34) / Math.pow(x.length, 0.2);
-        gaussian = new GaussianDistribution(0, h);
+        double iqr = 1./2. * (x[n*3/4] - x[n/4]  +  y[n*3/4] - y[n/4]);
+        h = 1.06 * Math.min( Math.sqrt(sdy*sd), iqr/1.34) / Math.pow(x.length, 0.2);
+        
+        //gaussian = new GaussianDistribution(0, h);
+        myRdd = sc.parallelize(Arrays.asList(this.data));
+        
+    }
+
+    public KernelDensity(JavaSparkContext sc,double[][] data ,double[]  weight_ ) {          // weight overloading
+        
+        
+        double weightSum = 0;
+        double[] x = new double[data.length];
+        double[] y = new double[data.length];
+        
+        double[][] data_ = new double[data.length][3];
+
+        for (int i =0 ;i<data.length; i++){
+            x[i]=data[i][0];
+            y[i]=data[i][1];
+            data_[i][0] = data[i][0];
+            data_[i][1] = data[i][1];
+            data_[i][2] = weight_[i];
+            weightSum += weight_[i];
+        }
+        this.x = x;
+        this.y = y;
+
+        this.weight_sum = weightSum;
+
+        this.data = data_;
+        this.mean = mean(x);
+        this.variance = Math.pow(sd(x),2);
+        this.sd = Math.sqrt(variance);
+        this.variancey = Math.pow(sd(y),2);
+        this.sdy = Math.sqrt(variancey);
+        
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        int n = x.length;
+        double iqr = 1./2. * (x[n*3/4] - x[n/4]  +  y[n*3/4] - y[n/4]);
+        h = 1.06 * Math.min( Math.sqrt(sdy*sd), iqr/1.34) / Math.pow(x.length, 0.2);
+        //h = 1.06 *  Math.sqrt(sdy*sd) / Math.pow(x.length, 0.2);
+        
+        
+        myRdd = sc.parallelize(Arrays.asList(this.data));
+        
     }
 
     /**
@@ -68,27 +145,65 @@ public class KernelDensity  {
      * @param x the samples to estimate the density function.
      * @param h a bandwidth parameter for smoothing.
      */
-    public KernelDensity(double[][] data, double h) {
+    public KernelDensity(JavaSparkContext sc, double[][] data, double h) {
         if (h <= 0) {
             throw new IllegalArgumentException("Invalid bandwidth: " + h);
         }
+        double[][] data_ = new double[data.length][3];
         double[] x = new double[data.length];
         double[] y = new double[data.length];
         for (int i =0 ;i<data.length; i++){
             x[i]=data[i][0];
             y[i]=data[i][1];
+            data_[i][0] = data[i][0];
+            data_[i][1] = data[i][1];
+            data_[i][2] = 1;
         }
         this.x = x;
         this.y = y;
         this.h = h;
-        this.mean = mean(x);
+        this.weight_sum = data.length;
+        this.data = data_;
+        this.mean = mean(x);    // mean of vector
         this.variance = Math.pow(sd(x),2);
         this.sd = Math.sqrt(variance);
-        gaussian = new GaussianDistribution(0, h);
-
+       
+        myRdd = sc.parallelize(Arrays.asList(this.data));
         Arrays.sort(x);
         Arrays.sort(y);
     }
+
+    public KernelDensity(JavaSparkContext sc, double[][] data, double h , double[] weight_) {  // weight overloading
+        if (h <= 0) {
+            throw new IllegalArgumentException("Invalid bandwidth: " + h);
+        }
+        
+        double weightSum = 0;
+        double[][] data_ = new double[data.length][3];
+        double[] x = new double[data.length];
+        double[] y = new double[data.length];
+        for (int i =0 ;i<data.length; i++){
+            x[i]=data[i][0];
+            y[i]=data[i][1];
+            data_[i][0] = data[i][0];
+            data_[i][1] = data[i][1];
+            data_[i][2] = weight_[i];
+            weightSum += weight_[i];
+        }
+        this.x = x;
+        this.y = y;
+        this.h = h;
+        this.weight_sum = weightSum;
+        this.data = data_;
+        this.mean = mean(x);    // mean of vector
+        this.variance = Math.pow(sd(x),2);
+        this.sd = Math.sqrt(variance);
+       // gaussian = new GaussianDistribution(0, h);
+        myRdd = sc.parallelize(Arrays.asList(this.data));
+        Arrays.sort(x);
+        Arrays.sort(y);
+    }
+  
 
 
 
@@ -120,15 +235,29 @@ public class KernelDensity  {
 
         
         double p = 0.0;
-        for (int i = start; i < end; i++) {
-            for (int j = starty; j<end; j++){
-                double norm =  Math.pow( this.x[i]-x ,2) + Math.pow( this.y[j]-y ,2) ;
-                p += gaussian.p( norm );
-            }
-        }
 
-        return p / (this.x.length *this.y.length);
+  /*  
+        for (int i = 0; i < 100; i++) {
+           
+            //double gausDist =  gaussian.p( this.x[i]-x ) * gaussian.p( this.y[j]-y ) ;
+            p +=  Math.exp(-1/(2*h*h)*( Math.pow( this.x[i] - x ,2) + Math.pow( this.y[i]-y ,2) ) ) / (2*Math.PI * h*h );
+            
+        }
+        
+*/
+        double h1 = this.h;
+        double weightSum = this.weight_sum;
+
+        p = myRdd.map(myarray->          
+            1000000000*(myarray[2]/weightSum)* Math.exp(-1/(2*h1*h1)*( Math.pow( myarray[0] - x ,2) + Math.pow( myarray[1]-y ,2) ) ) / (2*Math.PI * h1*h1 )   // weight
+        ).reduce((a,b)-> a+b);
+        
+        
+
+        return p / (this.x.length );
     }
+
+
 
     
     public static double mean(double[] m) {
--- src/kde_covid_kor/test/MultiKDEtest.java
+++ src/kde_covid_kor/test/MultiKDEtest.java
@@ -1,58 +1,26 @@
 package kde_covid_kor.test;
 
-import kde_covid_kor.covidDB.*;
+import java.util.Arrays;
 
-import java.util.ArrayList;
-
-import org.apache.log4j.Level;
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.examples.mllib.KernelDensityEstimationExample;
-import org.apache.spark.graphx.*;
-import org.apache.spark.mllib.stat.KernelDensity;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.Dataset;
-import org.apache.spark.sql.Row;
-import org.apache.spark.sql.SparkSession;
-import org.apache.spark.api.java.JavaSparkContext;
-
-import org.apache.log4j.Logger;
-
-import java.util.List;
-import java.util.ArrayList;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.mllib.stat.KernelDensity;
 
 public class MultiKDEtest {
-    public static void main(String[] args){
-        SparkSession spark = SparkSession
-            .builder()
-            .appName("covid_kde")
-            .config("spark.master","local")
-            .getOrCreate();
-        JavaSparkContext sc = new JavaSparkContext(spark.sparkContext());
-
-        TrajectoryData data = new TrajectoryData();
-        data.loadTrajectories();
-        Dataset<Trajectory> trajectory = data.getTrajectories();
-
-
-        trajectory.show();
-
-        System.out.print("hhhh");
-
-        trajectory.printSchema();
-
-        spark.close();
-
-    }
-
-    public void KDE(){
-
-    }
-
-    public void estimate(){
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("JavaKernelDensityEstimationExample").setMaster("local");
+        JavaSparkContext jsc = new JavaSparkContext(conf);
+        // an RDD of sample data
+        JavaRDD<Double> data = jsc.parallelize(Arrays.asList(1.0, 1.0, 1.0, 2.0, 3.0, 4.0, 5.0, 5.0, 6.0, 7.0, 8.0, 9.0, 9.0));
+        // and a standard deviation for the Gaussian kernels
+        KernelDensity kd = new KernelDensity().setSample(data).setBandwidth(3.0);
+        // Find density estimates for the given values
+        double[] densities = kd.estimate(new double[] { -1.0, 2.0, 5.0 });
+        System.out.println(Arrays.toString(densities));
         
-
+        // $example off$
+        jsc.stop();
+            
     }
-   
 }
